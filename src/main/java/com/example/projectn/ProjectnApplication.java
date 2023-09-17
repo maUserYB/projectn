01c2d554d95b84aca2218c74598823bedb1cd035
maUserYB <473561714@qq.com>
2023-09-17 08:43:49 +0800

    dd

---
+++ src/main/java/com/example/projectn/ProjectnApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectnApplication.class, args);
+    }
+
+}
